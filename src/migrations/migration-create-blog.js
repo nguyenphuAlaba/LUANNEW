df06dcf548ec6cb814070f7dd9b7fb37cb31346e
DaneNguyen <anhdansgvn@gmail.com>
2022-10-25 12:07:18 +0700

    phunguyen

---
+++ src/migrations/migration-create-blog.js
@@ -0,0 +1,37 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Blog", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      Description: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      img: {
+        type: Sequelize.TEXT,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Blog");
+  },
+};
