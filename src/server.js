39a4ad7f6ca160b0d5de0dbeef71a85f0bac0efa
DaneNguyen <anhdansgvn@gmail.com>
2022-12-26 23:41:33 +0700

    server

--- src/server.js
+++ src/server.js
@@ -1,47 +1,51 @@
-import express from "express";
+// import express from "express";
+var express = require("express");
+var cors = require("cors");
+var app = express();
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
 require("dotenv").config();
 
-let app = express();
+// let app = express();
+app.use(cors({ credentials: true, origin: true }));
 
 // enable CROS //
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:3002",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+// app.use(function (req, res, next) {
+//   const allowedOrigins = [
+//     "http://localhost:3000",
+//     "http://localhost:3001",
+//     "http://localhost:3002",
+//   ];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
 
-  // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'https://spotifakeplus.herokuapp.com');
+//   // Website you wish to allow to connect
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://spotifakeplus.herokuapp.com');
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, authorization"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, authorization"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 // config app //
 
